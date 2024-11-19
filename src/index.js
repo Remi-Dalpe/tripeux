d9af5bc4fe4a8aecfee0aa840174a81f2e8a9532
Remi-Dalpe <remidalpe5@gmail.com>
2024-11-19 00:59:01 -0500

    v0.1.0-alpha9

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-  console.log('Analytics initialized');
+  console.log('Analytics initialized', analytics);
 
   // Dynamically import other modules
   await Promise.all([
